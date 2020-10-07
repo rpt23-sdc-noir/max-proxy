b4aecb60b33d71a049b14ee20559e3fa00580b59
Nicholas Tolle <nbtolle@gmail.com>
2020-10-06 21:11:26 -0600

    Get component to render through proxy Some changes were made since successful rendering.  To get rendering to work properly, comment out line 7 in index.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const {createProxyMiddleware} = require('http-proxy-middleware');
 const app = express();
+const path = require('path');
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/relatedTracks', createProxyMiddleware({
+  target: 'http://localhost:3001',
+  changeOrigin: true
+}));
+
+app.use('/', createProxyMiddleware({
   target: 'http://localhost:3001',
   changeOrigin: true
 }));
