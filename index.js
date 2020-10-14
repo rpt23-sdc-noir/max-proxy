5b7d6ebb5246a5344d9cbfb0fc1b87d60b101c52
Nicholas Tolle <nbtolle@gmail.com>
2020-10-13 20:17:19 -0600

    Get all services rendered to DOM Also installed nodemon as a dependency for better server handling.

--- index.js
+++ index.js
@@ -4,41 +4,37 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-app.use('/relatedTracks', createProxyMiddleware({
-  target: 'http://localhost:3001',
-  changeOrigin: true
-}));
-
-app.use('/', createProxyMiddleware({
-  target: 'http://localhost:3001',
-  changeOrigin: true
-}));
-
-app.use('/comments', createProxyMiddleware({
-  target: 'http://localhost:4000',
-  changeOrigin: true
-}));
-
-app.use('/hashtags', createProxyMiddleware({
-  target: 'http://localhost:4000',
-  changeOrigin: true
-}));
-
-app.use('/users', createProxyMiddleware({
-  target: 'http://localhost:4000',
-  changeOrigin: true
-}));
-
-app.use('/artistBio', createProxyMiddleware({
-  target: 'http://localhost:2000',
-  changeOrigin: true
-}));
-
-app.use('/songdata', createProxyMiddleware({
+app.use('/songdata/', createProxyMiddleware({
   target: 'http://localhost:1000',
   changeOrigin: true
 }));
 
+app.use('/relatedTracks/', createProxyMiddleware({
+  target: 'http://localhost:3001',
+  changeOrigin: true
+}))
+
+app.use('/artistBio/', createProxyMiddleware({
+  target: 'http://localhost:2000',
+  changeOrigin: true
+}))
+
+app.use('/hashtags/', createProxyMiddleware({
+  target: 'http://localhost:9001',
+  changeOrigin: true
+}))
+
+app.use('/', createProxyMiddleware({
+  target: 'http://localhost:1000',
+  changeOrigin: true
+}))
+
+app.use('/', createProxyMiddleware({
+  target: 'http://localhost:1000',
+  changeOrigin: true
+}))
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
